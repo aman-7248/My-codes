378d8ee84426d3d77fe67d3c70c673018e322757
aman-7248 <127982352+aman-7248@users.noreply.github.com>
2024-07-04 23:24:25 +0530

    Java

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test{
+    public static int sum(int a,int b){
+        int sum1=a^b;
+        return sum1;
+    }
+    public static void main(String args[]){
+        System.out.printl("Hello :)");
+        System.out.println("sum is :" + sum(2,100));
+    }
+}
